48c0ace44d6be82c1f0388b2d3152023b018cd9a
lokmankhodziri <lokmankhodziri@gmail.com>
2023-05-28 03:42:36 +0800

    new update

--- src/component/routes/shop/shop.component.jsx
+++ src/component/routes/shop/shop.component.jsx
@@ -1,19 +1,23 @@
 import { useContext } from 'react';
 
-import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../product-card/product-card.component';
+import { CategoriesContext } from '../../contexts/categories.context';
+import CategoryPreview from '../../category-preview/category-preview.component';
 
 import './shop.styles.scss'
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext);
+    const { categoriesMap } = useContext(CategoriesContext);
+
     return (
-        <div className='product-container'>
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title];
+                return (
+                    <CategoryPreview key={title} title={title} products={products} />
+                );
+            })}
         </div>
-    )
-}
+    );
+};
 
 export default Shop;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './component/contexts/user.context';
-import { ProductProvider } from './component/contexts/products.context';
+import { CategoriesProvider } from './component/contexts/categories.context';
 import { CartProvider } from './component/contexts/cart.context';
 
 import './index.scss';
@@ -18,11 +18,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
